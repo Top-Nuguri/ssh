39d5f8be105ca43c2d69ce0b004db87cca7373e1
ssh <shsh9971@naver.com>
2024-06-14 02:07:49 +0900

    Merge branch 'master' of https://github.com/Top-Nuguri/ssh

--- motor.py
+++ motor.py
@@ -1,5 +1,45 @@
-from ev3dev.auto import *
-import time
+import sys
+import msvcrt
+from ev3dev.ev3 import *
 
-m = Motor(OUTPUT_B)
-m.run_timed(time_sp=3000, speed_sp=500)
+motor_left = LargeMotor('outB')
+motor_right = LargeMotor('outC')
+
+def getch():
+    return msvcrt.getch().decode('utf-8')
+
+def forward():
+    motor_left.run_forever(speed_sp=450)
+    motor_right.run_forever(speed_sp=450)
+
+def back():
+    motor_left.run_forever(speed_sp=-450)
+    motor_right.run_forever(speed_sp=-450)
+
+def left():
+    motor_left.run_forever(speed_sp=-450)
+    motor_right.run_forever(speed_sp=450)
+
+def right():
+    motor_left.run_forever(speed_sp=450)
+    motor_right.run_forever(speed_sp=-450)
+
+def stop():
+    motor_left.run_forever(speed_sp=0)
+    motor_right.run_forever(speed_sp=0)
+
+while True:
+    k = getch()
+    print(k)
+    if k == 'w':
+        forward()
+    if k == 's':
+        back()
+    if k == 'a':
+        left()
+    if k == 'd':
+        right()
+    if k == ' ':
+        stop()
+    if k == 'q':
+        break
